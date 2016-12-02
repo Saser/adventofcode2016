ca56077c86c86ff59cbb5714b15313544c900e57
Christian Persson <saser@live.se>
2016-12-02 18:24:41 +0100

    Methods on Direction for calculating new Directions

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -10,6 +10,33 @@ enum Turn {
     Left,
 }
 
+impl Direction {
+    fn turn(&self, turn: Turn) -> Direction {
+        match turn {
+            Turn::Right => self.turn_right(),
+            Turn::Left => self.turn_left(),
+        }
+    }
+
+    fn turn_right(&self) -> Direction {
+        match *self {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        }
+    }
+
+    fn turn_left(&self) -> Direction {
+        match *self {
+            Direction::North => Direction::West,
+            Direction::East => Direction::North,
+            Direction::South => Direction::East,
+            Direction::West => Direction::South,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
