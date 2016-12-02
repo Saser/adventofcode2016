192193c8f355deea41c3b9d89658baf908f694d8
Christian Persson <saser@live.se>
2016-12-02 18:18:01 +0100

    Added some basic enums

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,6 +1,17 @@
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+enum Turn {
+    Right,
+    Left,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
