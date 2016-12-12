22cd7e32eff4d9b2f18a21ea2eab69e73f15b39c
Christian Persson <saser@live.se>
2016-12-12 22:16:38 +0100

    Add parsing of parts

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -1,6 +1,21 @@
+use std::str::FromStr;
+
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub enum Part {
     One,
     Two,
+}
+
+impl FromStr for Part {
+    type Err = String;
+
+    fn from_str(part_str: &str) -> Result<Self, Self::Err> {
+        match part_str.to_lowercase().as_str() {
+            "1" | "one" => Ok(Part::One),
+            "2" | "two" => Ok(Part::Two),
+            _ => Err("invalid part specification".to_owned()),
+        }
+    }
 }
 
 pub trait ProblemSolver {
@@ -9,4 +24,34 @@ pub trait ProblemSolver {
     fn solve(input: &str, part: Part) -> String;
 
     fn solve_file(file_path: &str, part: Part) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_from_str() {
+        let p1_str1 = "one";
+        let p1_str2 = "1";
+        let p1 = Part::One;
+        assert_eq!(Part::from_str(p1_str1).unwrap(), p1);
+        assert_eq!(Part::from_str(p1_str2).unwrap(), p1);
+
+        let p2_str1 = "two";
+        let p2_str2 = "2";
+        let p2 = Part::Two;
+        assert_eq!(Part::from_str(p2_str1).unwrap(), p2);
+        assert_eq!(Part::from_str(p2_str2).unwrap(), p2);
+    }
+
+    #[test]
+    fn part_from_str_err() {
+        let err1 = "derp";
+        let err2 = "0ne";
+        let err3 = "3";
+        assert!(Part::from_str(err1).is_err());
+        assert!(Part::from_str(err2).is_err());
+        assert!(Part::from_str(err3).is_err());
+    }
 }
