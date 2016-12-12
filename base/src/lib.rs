dba8af054922d0158229e3d9d1703baedb269a9b
Christian Persson <saser@live.se>
2016-12-12 21:59:20 +0100

    Forgot that these need to have a Part specification

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -6,7 +6,7 @@ pub enum Part {
 pub trait ProblemSolver {
     fn new() -> Self;
 
-    fn solve(input: &str) -> String;
+    fn solve(input: &str, part: Part) -> String;
 
-    fn solve_file(file_path: &str) -> String;
+    fn solve_file(file_path: &str, part: Part) -> String;
 }
