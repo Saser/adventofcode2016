ee012e1a3537a6be86923b0336318921370a89f3
Christian Persson <saser@live.se>
2016-12-12 21:43:34 +0100

    Basic configuration of arguments

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,3 +1,12 @@
+#[macro_use]
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    println!("Value of day: {}", matches.value_of("day").unwrap());
+    println!("Value of part: {}", matches.value_of("part").unwrap());
+    println!("Value of file: {}", matches.value_of("file").unwrap());
 }
