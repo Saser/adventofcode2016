7848c8e34b9d2c189fdff5adf321ee67da06edd7
Christian Persson <saser@live.se>
2016-12-23 17:22:03 +0100

    Add some more boilerplate

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -5,12 +5,28 @@ struct Solver;
 
 impl ProblemSolver for Solver {
     fn solve(&self, input: &str, part: &Part) -> Result<String, String> {
-        Err("not implemented yet!".to_owned())
+        match *part {
+            Part::One => solve_part_one(&input),
+            Part::Two => solve_part_two(&input),
+        }
     }
 
     fn solve_file(&self, file_path: &str, part: &Part) -> Result<String, String> {
-        Err("not implemented yet!".to_owned())
+        let lines = base::utils::lines_from_file(file_path);
+        let input = &lines[0];
+        match *part {
+            Part::One => solve_part_one(input),
+            Part::Two => solve_part_two(input),
+        }
     }
+}
+
+fn solve_part_one(input: &str) -> Result<String, String> {
+    Err("not implemented yet!".to_owned())
+}
+
+fn solve_part_two(input: &str) -> Result<String, String> {
+    Err("not implemented yet!".to_owned())
 }
 
 #[cfg(test)]
