b93caab268acf0770820dded89a5d6281b36bda2
Christian Persson <saser@live.se>
2016-12-23 17:18:18 +0100

    Add base crate and set up some boilerplate

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,3 +1,18 @@
+extern crate base;
+use base::{Part, ProblemSolver};
+
+struct Solver;
+
+impl ProblemSolver for Solver {
+    fn solve(&self, input: &str, part: &Part) -> Result<String, String> {
+        Err("not implemented yet!".to_owned())
+    }
+
+    fn solve_file(&self, file_path: &str, part: &Part) -> Result<String, String> {
+        Err("not implemented yet!".to_owned())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
