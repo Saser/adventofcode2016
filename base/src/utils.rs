47f209f719a83b865c8808f6bbf7be8fd1a3c52f
Christian Persson <saser@live.se>
2016-12-25 00:35:54 +0100

    Update the functions for reading a file a bit

--- base/src/utils.rs
+++ base/src/utils.rs
@@ -2,14 +2,20 @@
 //! reduce the amount of boilerplate code related to e.g. reading input.
 
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufReader, Read};
 
 /// Takes a file path and returns a `Vec` of all the lines, without any trailing newline
 /// characters. Assumes that all lines in the file are readable as strings.
 pub fn lines_from_file(file_path: &str) -> Vec<String> {
+    read_file_as_string(file_path).split('\n').map(str::to_owned).collect()
+}
+
+/// Takes a file path and reads the contents of that file, assuming that it is a text file. The
+/// read contents are returned as a new `String`.
+pub fn read_file_as_string(file_path: &str) -> String {
     let file = File::open(file_path).unwrap();
-    let reader = BufReader::new(&file);
-    reader.lines()
-        .map(|line| line.unwrap())
-        .collect()
+    let mut reader = BufReader::new(&file);
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf);
+    buf
 }
