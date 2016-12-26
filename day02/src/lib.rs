c2f3132e3ade01fcf114f76ae870ad02f51a598f
Christian Persson <saser@live.se>
2016-12-26 23:42:16 +0100

    Begin day 2 by creating a normal keypad

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,5 +1,6 @@
 extern crate base;
 use base::{Part, ProblemSolver};
+use base::coord::Position;
 
 pub fn get_solver() -> Box<ProblemSolver> {
     Box::new(Solver)
@@ -10,5 +11,28 @@ struct Solver;
 impl ProblemSolver for Solver {
     fn solve(&self, input: &str, part: Part) -> Result<String, String> {
         unimplemented!()
+    }
+}
+
+trait Keypad {
+    fn press(position: Position) -> Option<String>;
+}
+
+struct StandardKeypad;
+
+impl Keypad for StandardKeypad {
+    fn press(position: Position) -> Option<String> {
+        match position {
+            Position(-1, 1) => Some(1.to_string()),
+            Position(0, 1) => Some(2.to_string()),
+            Position(1, 1) => Some(3.to_string()),
+            Position(-1, 0) => Some(4.to_string()),
+            Position(0, 0) => Some(5.to_string()),
+            Position(1, 0) => Some(6.to_string()),
+            Position(-1, -1) => Some(7.to_string()),
+            Position(0, -1) => Some(8.to_string()),
+            Position(1, -1) => Some(9.to_string()),
+            _ => None,
+        }
     }
 }
